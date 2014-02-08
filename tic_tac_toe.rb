dd4ba01dbe9f83e41af621b36e5031397971fc01
Brian Fitch <brianmfitch@gmail.com>
2014-02-08 01:40:33 -0500

    hacking

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,5 +1,5 @@
-require "observer"
-require "set"
+require 'observer'
+require 'pp'
 
 class TicTacToe
   def initialize
@@ -8,65 +8,14 @@ class TicTacToe
     screen.read
   end
 
-  def update(args)
-    position, value = args.split(' ')
-    puts "=> #{position}, #{value}"
-
-    cell  = Cell.new(position, value)
-    board = Board.new.set(cell)
-    p board
+  def update(coordinate, value)
+    coordinate = Coordinate.new *coordinate.scan(/\w/)
+    cell       = Cell.new(coordinate, value)
+    board      = Board.new.set(cell)
+    pp board
     # screen.draw View.set board
   end
 end
-
-#|----|----|----|
-#|    |    |    |
-#|----|----|----|
-#|    |    |    |
-#|----|----|----|
-#|    |    |    |
-#|----|----|----|
-
-class Cell
-  attr_reader :position
-
-  def initialize(position, value)
-    @position = position
-    @value    = to_boolean(value)
-  end
-
-  def to_boolean(value)
-    if value == "x"
-      return true
-    else
-      return false
-    end
-  end
-
-  def ==(object)
-    position == object.position
-  end
-end
-
-
-class Board
-  POSITIONS = [ '02', '12', '22',
-                '01', '11', '21',
-                '00', '10', '20' ]
-
-  def set(cell)
-    build
-    @board << cell if @board.delete(cell)
-  end
-
-  def build
-    @board = POSITIONS.inject([]) do |acc, position |
-      acc << Cell.new(position, false)
-      acc
-    end
-  end
-end
-
 
 class Screen
   include Observable
@@ -77,8 +26,9 @@ class Screen
    
   def read
     loop do
+      coordinate, value = gets.split(' ')
       changed 
-      notify_observers(gets.chomp)
+      notify_observers(coordinate, value.chomp)
     end
     sleep 1
   end
@@ -86,6 +36,97 @@ class Screen
   def draw(data)
     puts data
   end
+end
+
+#|----|----|----|
+#|    |    |    |
+#|----|----|----|
+#|    |    |    |
+#|----|----|----|
+#|    |    |    |
+#|----|----|----|
+
+class Board
+  include Enumerable
+
+  PAIRS = [ '0c', '1c', '2c',
+            '0b', '1b', '2b',
+            '0a', '1a', '2a' ]
+
+  def set(cell)
+    build
+    @board << cell if @board.delete(cell)
+  end
+
+  def build
+    @board = coordinates.each_with_object([]) do |coordinate, acc|
+      acc << Cell.new(coordinate, false)
+    end
+  end
+
+  def each
+    build.each { |cell| yield cell }
+  end
+
+  private
+
+  def coordinates
+    PAIRS.map { |coordinate| Coordinate.new(*coordinate.scan(/\w/)) }
+  end
+end
+
+
+class Cell
+  attr_reader :coordinate
+
+  def initialize(coordinate, value)
+    @coordinate = coordinate
+    @value      = to_boolean(value)
+  end
+
+  def to_boolean(value)
+    if value == "x"
+      return true
+    else
+      return false
+    end
+  end
+
+  def <=>(object)
+    coordinate <=> object.coordinate
+  end
+end
+
+class Coordinate
+  include Comparable
+
+  attr_reader :x, :y
+
+  def initialize(x,y)
+    @x, @y = x,y
+  end
+
+  def <=>(object)
+    if (x == object.x) && (y == object.y)
+      0
+    elsif (x >= object.x) && (y >= object.y)
+      1
+    else
+      -1
+    end
+  end
+
+    # if (x > object.x) && (y > object.y)
+    #   1
+    # elsif (x > object.x) && (y >= object.y)
+    #   1
+    # elsif (x >= object.x) && (y > object.y)
+    #   1
+    # elsif (x == object.x) && (y == object.y)
+    #   0
+    # else
+    #   -1
+    # end
 end
 
 TicTacToe.new
